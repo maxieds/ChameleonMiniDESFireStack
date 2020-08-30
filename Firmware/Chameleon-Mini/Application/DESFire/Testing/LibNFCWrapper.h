53629586fc98dbff30a78e4288fa682d5c22798f
Maxie D. Schmidt <maxieds@gmail.com>
2020-08-30 19:28:54 -0400

    More testing development files

--- Firmware/Chameleon-Mini/Application/DESFire/Testing/LibNFCWrapper.h
+++ Firmware/Chameleon-Mini/Application/DESFire/Testing/LibNFCWrapper.h
@@ -14,30 +14,36 @@
 
 #include <nfc/nfc.h>
 
+#include "LibNFCUtils.h"
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif // HAVE_CONFIG_H
+
 static inline nfc_device * GetNFCDeviceDriver(nfc_context **context) {
     nfc_init(context);
     if(context == NULL) {
         ERR("Unable to init libnfc (malloc)");
         return NULL;
-    }
+    }   
     nfc_device *pnd = nfc_open(*context, NULL);
     if(pnd == NULL) {
         ERR("Error opening NFC reader");
         nfc_exit(context);
         return NULL;
-    }
+    }   
     if(nfc_initiator_init(pnd) < 0) {
         nfc_perror(pnd, "nfc_initiator_init");
         nfc_close(pnd);
         nfc_exit(context);
         return NULL;
-    }
+    }   
     // Configure some convenient common settings:
-    //nfc_pnd_set_property_bool(pnd, NP_ACTIVATE_FIELD, false);
-    //nfc_pnd_set_property_bool(pnd, NP_HANDLE_CRC, true);
-    //nfc_pnd_set_property_bool(pnd, NP_HANDLE_PARITY, true);
-    //nfc_pnd_set_property_bool(pnd, NP_AUTO_ISO14443_4, true);
-    //nfc_pnd_set_property_bool(pnd, NP_ACTIVATE_FIELD, true);
+    nfc_device_set_property_bool(pnd, NP_ACTIVATE_FIELD, false);
+    nfc_device_set_property_bool(pnd, NP_HANDLE_CRC, true);
+    nfc_device_set_property_bool(pnd, NP_HANDLE_PARITY, true);
+    nfc_device_set_property_bool(pnd, NP_AUTO_ISO14443_4, true);
+    nfc_device_set_property_bool(pnd, NP_ACTIVATE_FIELD, true);
     return pnd;
 }
 
